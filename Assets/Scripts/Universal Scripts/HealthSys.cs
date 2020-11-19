a0b79e854ef8d10d9131568291f444c07f9a1056
Resolas <64108044+Resolas@users.noreply.github.com>
2020-11-19 20:28:57 +0000

    Added BossTestScene

---
+++ Assets/Scripts/Universal Scripts/HealthSys.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthSys : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public int health;
+    public bool isDead;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        onDeath();
+    }
+
+    void onDeath()
+    {
+        if (health <= 0)
+        {
+            isDead = true;
+        }
+
+    }
+}
