69d3b0f36f17d9139e67c8e145aecc3ec18e0d5a
Resolas <64108044+Resolas@users.noreply.github.com>
2020-11-22 18:12:36 +0000

    Added ProjectileSys and Updated BossScript
    - ProjectileSys is a modular script to used to make various projectiles and melee attacks
    
    - BossController is setup but no functioning AI yet

--- Assets/Scripts/Morrigan/MorriganBossController.cs
+++ Assets/Scripts/Morrigan/MorriganBossController.cs
@@ -2,17 +2,58 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[RequireComponent(typeof(HealthSys))]
 public class MorriganBossController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        getBossHealth = GetComponent<HealthSys>();
     }
+    [SerializeField]
+    private float distToTarget;
+    [SerializeField]
+    private HealthSys getBossHealth;
+
+    [Header("Sense Setting")]
+    public bool isTargetVis;
+    public bool isTargetHear;
+    public bool isTargetClose;
+
+    [Header("Attack Settings")]
+    public GameObject[] attacksTypes;
+    public GameObject[] firePointsStage1;
+    public GameObject[] firePointsStage2;
+    public GameObject[] firePointsStage3;
+
+    public GameObject[] firePointSubAttack;
+    private int attackMode;                 // Attack Modes: 1 = Firepoints-1, 2 = Firepoints-2 etc...      4 = Firepoints 1-3 (Fires All Stages)
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+
+        Movement();
+        AIDecision();
+
     }
+
+    void Movement()
+    {
+
+    }
+
+    void AIDecision()
+    {
+
+    }
+
+    void LaunchAttacks()
+    {
+
+
+    }
+
+
 }
---
+++ Assets/Scripts/Universal Scripts/ProjectileSys.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Collider2D))]
+public class ProjectileSys : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        myCollider = GetComponent<Collider2D>();
+        if (isMultiHit) myCollider.isTrigger = true;
+        else myCollider.isTrigger = false;
+
+    }
+
+    private Collider2D myCollider;
+
+    [Header("Settings")]
+    public int damage = 10;
+    public float speed = 5f;
+    public float lifetime = 5f;
+    public float delay = 0;     //  stays still until delay is up
+    public bool isHoming = false;       // Tracks and chases target
+    public bool isMultiHit = false;             // Will not despawn on hit
+    public GameObject subProjectile;                    // Fires projectile out of main projectile
+    public bool isExplosive = false;                            // Does exactly what it says
+    public int explosiveDamage = 5;
+    public float radius = 5;
+
+    [Header("Effects")]
+    public GameObject[] effectOnHit;
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+
+        lifetime -= 1 * Time.deltaTime;
+
+        if (lifetime <= 0) Destroy(gameObject);
+
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+
+        if (collision.collider.CompareTag("Player") || collision.collider.CompareTag("Enemy"))
+        {
+            int otherHP = collision.collider.GetComponent<HealthSys>().health;
+
+            otherHP -= damage;
+
+
+        }
+
+        
+
+        for (int i = 0; i < effectOnHit.Length; i++)
+        {
+            if (effectOnHit != null)
+            Instantiate(effectOnHit[i],transform.position,Quaternion.identity);
+        }
+
+        Destroy(gameObject);
+
+    }
+
+    private void OnTriggerStay2D(Collider2D other)  // will pass through targets
+    {
+        if (isMultiHit)
+        {
+
+            if (other.CompareTag("Player") || other.CompareTag("Enemy"))
+            {
+
+                int otherHP = other.GetComponent<HealthSys>().health;
+
+                otherHP -= damage;
+
+            }
+
+
+
+
+        }
+    }
+
+    private void Explode()
+    {
+        Vector2 myPos = transform.position;
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(myPos,radius);
+        foreach (Collider2D hit in colliders)
+        {
+
+            if (hit.CompareTag("Player") || hit.CompareTag("Enemy"))
+            {
+
+               int otherHP = hit.GetComponent<HealthSys>().health;
+
+                otherHP -= explosiveDamage;
+               
+            }
+
+        }
+
+    }
+    
+
+    private void OnDestroy()
+    {
+        for (int i = 0; i < effectOnHit.Length; i++)
+        {
+
+            Instantiate(effectOnHit[i], transform.position, Quaternion.identity);
+        }
+    }
+}
