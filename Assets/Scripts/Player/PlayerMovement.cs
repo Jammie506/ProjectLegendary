5eec8a1532cc5949a3d71434437eeda6a1482776
PhilipVDL <61231557+PhilipVDL@users.noreply.github.com>
2020-11-23 14:14:44 +0000

    Player dodge

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -9,11 +9,29 @@ public class PlayerMovement : MonoBehaviour
 
     //variables
     public float speed;
+    public float dodgeSpeed;
+    public float dodgeTime;
+
     public bool movementStyle;
+
+    public bool moving;
+    public bool dodging;
     private float horizontal;
     private float vertical;
+    private Vector3 direction;
 
     private void Update()
+    {
+        MoveCheck();
+        Dodge();
+
+        if (!dodging)
+        {
+            Movement();
+        }
+    }
+
+    void Movement()
     {
         if (movementStyle)
         {
@@ -34,6 +52,10 @@ public class PlayerMovement : MonoBehaviour
         //translate
         transform.Translate(horizontal * speed * Time.deltaTime, 0, 0);
         transform.Translate(0, vertical * speed * Time.deltaTime, 0);
+
+        //dodge Vector
+        Vector3 dualVector = new Vector3(horizontal, vertical, 0);
+        direction = dualVector.normalized;
     }
 
     void MovementVectoredAxes()
@@ -44,9 +66,45 @@ public class PlayerMovement : MonoBehaviour
 
         //Normalized Vector
         Vector3 dualVector = new Vector3(horizontal, vertical, 0);
-        Vector3 direction = dualVector.normalized;
+        direction = dualVector.normalized;
 
         //translate
         transform.Translate(direction * speed*Time.deltaTime);
+    }
+
+    void MoveCheck()
+    {
+        if(Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+        {
+            moving = true;
+        }
+        else
+        {
+            moving = false;
+        }
+    }
+
+    void Dodge()
+    {
+        if (Input.GetButtonDown("Jump") && !dodging && moving)
+        {
+            dodging = true;
+            float dodgeTimer = dodgeTime;
+            StartCoroutine(DodgeMove(dodgeTimer));
+        }
+    }
+
+    IEnumerator DodgeMove(float timer)
+    {
+        float dodgeRoutine = timer;
+
+        while(dodgeRoutine > 0)
+        {
+            dodgeRoutine -= Time.deltaTime;
+            transform.Translate(direction * dodgeSpeed * Time.deltaTime);
+            yield return null;
+        }
+
+        dodging = false;
     }
 }
