0e5270ea43b5068e4c5c49e5313ce9920e756121
Resolas <64108044+Resolas@users.noreply.github.com>
2020-11-29 16:53:20 +0000

    Poof Effect

---
+++ Assets/Scripts/Morrigan/OnEnableEffect.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnEnableEffect : MonoBehaviour
+{
+    
+
+    public GameObject myEffect;
+
+    private void OnEnable()
+    {
+        Instantiate(myEffect,transform.position,Quaternion.identity);
+    }
+
+    private void OnDisable()
+    {
+        Instantiate(myEffect, transform.position, Quaternion.identity);
+    }
+
+
+}
