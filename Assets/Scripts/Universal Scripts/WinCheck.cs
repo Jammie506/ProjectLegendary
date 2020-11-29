d17d6416b7ac2aebba24974fae4ec280210c4319
PhilipVDL <61231557+PhilipVDL@users.noreply.github.com>
2020-11-29 16:24:15 +0000

    Win Screen

---
+++ Assets/Scripts/Universal Scripts/WinCheck.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinCheck : MonoBehaviour
+{
+    private void Update()
+    {
+        Win();
+    }
+
+    void Win()
+    {
+        if(GameObject.FindGameObjectWithTag("Enemy") == null)
+        {
+            SceneManager.LoadSceneAsync("GameWin");
+        }
+    }
+}
