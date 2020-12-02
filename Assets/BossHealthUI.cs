de801dca52526ef5398cdf4dbd03b112b8cbb3ca
PhilipVDL <61231557+PhilipVDL@users.noreply.github.com>
2020-12-02 14:31:49 +0000

    Boss health displayed

---
+++ Assets/BossHealthUI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BossHealthUI : MonoBehaviour
+{
+    HealthSys bossHealth;
+    Text hp;
+
+    private void Start()
+    {
+        bossHealth = GameObject.Find("BossUnit").GetComponent<HealthSys>();
+        hp = GetComponent<Text>();
+        Debug.Log(hp);
+    }
+
+    private void Update()
+    {
+        if(bossHealth != null)
+        {
+            hp.text = "Boss Health: " + bossHealth.health;
+        }
+    }
+}
