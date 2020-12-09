adfc34d89288ce5bb6d5bdfe8ee4e66a2d260ca6
Resolas <64108044+Resolas@users.noreply.github.com>
2020-12-09 00:52:58 +0000

    HitTrigger and Vlambeer/Screenshake

---
+++ Assets/Scripts/Player/HitTrig.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HitTrig : MonoBehaviour
+{   // If Target Health is lowered such as player's health, if its lowered than its newly registered value it will trigger
+
+
+    #region  Variables
+
+    [Header("Settings")]
+    public Image myFlashImage;
+    public Vlambeer myVlambeer;
+    public GameObject myHitEffect;
+    public AudioClip myHitAudio;
+
+    private HealthSys myObjHealth;
+    private AudioSource mySource;
+    int newHPValue;
+    #endregion
+
+    private void Start()
+    {
+        myObjHealth = GetComponent<HealthSys>();
+        mySource = GetComponent<AudioSource>();
+
+        newHPValue = myObjHealth.health;
+    }
+
+
+
+    private void Update()
+    {
+
+
+        if (newHPValue > myObjHealth.health)
+        {
+            Debug.Log("HIT TEST");
+            TriggerHitEffects();
+
+            newHPValue = myObjHealth.health;
+        }
+
+            
+        
+    }
+
+    void TriggerHitEffects()
+    {
+        
+        if (myVlambeer != null) myVlambeer.ShakeScreen(1);
+
+        //    if (myFlashImage != null)
+
+        if (myHitEffect != null) Instantiate(myHitEffect,transform.position,Quaternion.identity);
+
+        if (myHitAudio != null) mySource.PlayOneShot(myHitAudio);
+
+    }
+
+
+
+}
---
+++ Assets/Scripts/Player/Vlambeer.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vlambeer : MonoBehaviour
+{   // Its Screenshake
+    
+
+    [SerializeField]
+    GameObject myCamPos;
+    Vector2 getOriginPos;
+    private float xPos = 0, yPos = 0;
+    private float randX, randY;
+
+    [Header("Settings")]
+    public float shakeStrength = 1f;
+    public float fallOff = 2f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.P)) ShakeScreen(shakeStrength);      // For Testing Purposes
+
+        randX = Random.Range(-xPos,xPos);
+        randY = Random.Range(-yPos,yPos);
+
+
+        myCamPos.transform.Translate(new Vector2(randX,randY));
+
+        if (xPos > 0 || yPos > 0)
+        {
+            xPos -= fallOff * Time.deltaTime;
+            yPos -= fallOff * Time.deltaTime;
+
+            if (xPos >= 0.1f || yPos >= 0.1f)
+            {
+
+                Mathf.Round(xPos);
+                Mathf.Round(yPos);
+            }
+
+        }
+        else if (xPos < 0 || yPos < 0)
+        {
+            xPos = 0;
+            yPos = 0;
+        }
+
+    }
+
+    public void ShakeScreen(float intensity)        // To be triggered from other scripts
+    {
+
+        xPos = intensity;
+        yPos = intensity;
+        Debug.Log("SCREENSHAKE");
+    }
+
+}
