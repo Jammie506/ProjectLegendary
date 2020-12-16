5c7d214e075e3c78634b82aef3e6f2bfdf8f9760
PhilipVDL <61231557+PhilipVDL@users.noreply.github.com>
2020-12-16 13:00:44 +0000

    boss spawns after 15 seconds

---
+++ Assets/BossSpawnTime.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossSpawnTime : MonoBehaviour
+{
+    public float seconds;
+    private float timer;
+    public bool spawned;
+
+    GameObject boss;
+
+    private void Start()
+    {
+        timer = seconds;
+        boss = GameObject.Find("BossUnit");
+        boss.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+        }
+        else if (timer <= 0 && !spawned)
+        {
+            spawned = true;
+            boss.SetActive(true);
+        }
+    }
+}
