ed26117b78f2d5cd52ff0eed28b1377628c7f2eb
PhilipVDL <61231557+PhilipVDL@users.noreply.github.com>
2020-12-17 13:26:13 +0000

    Start screen

---
+++ Assets/Scripts/StartScreenScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScreenScript : MonoBehaviour
+{
+    public void PressPlay()
+    {
+        SceneManager.LoadSceneAsync("Game Scene");
+    }
+
+    public void PressQuit()
+    {
+        Application.Quit();
+    }
+}
