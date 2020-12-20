93c8e1a95a02a7dfcd7db2da6b12ab48a4f16035
Resolas <64108044+Resolas@users.noreply.github.com>
2020-12-20 19:57:41 +0000

    Hulk ScreenShake
    - Added screenshake to hulk mode attacks

---
+++ Assets/Scripts/Player/VlambeerOnCreate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VlambeerOnCreate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        getPlayer = GameObject.Find("Player");
+
+        getVlambeer = getPlayer.GetComponent<Vlambeer>();
+
+        getVlambeer.ShakeScreen(1);
+
+    }
+
+    GameObject getPlayer;
+    Vlambeer getVlambeer;
+
+    
+}
